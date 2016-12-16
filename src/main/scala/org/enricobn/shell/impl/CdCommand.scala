9e3f30a8d638640bb97301c203a97434e729db72
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-12-16 13:51:19 +0100

    Refactoring.

--- src/main/scala/org/enricobn/shell/impl/CdCommand.scala
+++ src/main/scala/org/enricobn/shell/impl/CdCommand.scala
@@ -1,6 +1,6 @@
 package org.enricobn.shell.impl
 
-import org.enricobn.shell.{ShellInput, ShellOutput, VirtualCommand}
+import org.enricobn.shell.{Completions, ShellInput, ShellOutput, VirtualCommand}
 import org.enricobn.vfs.{VirtualFolder, VirtualIOException}
 
 import scala.scalajs.js.annotation.JSExport
@@ -35,7 +35,7 @@ class CdCommand extends VirtualCommand {
     val start = parsedLine.lastArgument.getOrElse("")
 
     if (parsedLine.lastArgument.isDefined) {
-      val partialPath = resolveFolder(currentFolder, parsedLine.lastArgument.get)
+      val partialPath = Completions.resolveFolder(currentFolder, parsedLine.lastArgument.get)
       println(partialPath)
       if (partialPath.folder.isDefined) {
         if (partialPath.remaining.isDefined) {
@@ -62,50 +62,4 @@ class CdCommand extends VirtualCommand {
     }
   }
 
-  case class PartialPath(folder: Option[VirtualFolder], remaining: Option[String]) {
-    // TODO implement VirtualNode.toString with path (can be final ?)
-    override def toString: String = "(" + folder.map(_.path).getOrElse("None") + "," + remaining + ")"
-  }
-
-  private def resolveFolder(currentFolder: VirtualFolder, prefix: String) : PartialPath = {
-    println(prefix)
-    if (prefix.startsWith("/")) {
-      val lastSlash = prefix.lastIndexOf('/')
-      if (lastSlash == 0) {
-        PartialPath(Some(currentFolder.root), Some(prefix.substring(lastSlash + 1)))
-      } else {
-        try {
-          // TODO currentPermission must be handled by resolveFolder
-          PartialPath(Some(currentFolder.resolveFolder(prefix.substring(0, lastSlash))),
-            if (prefix.length == lastSlash -1) None else Some(prefix.substring(lastSlash +1)))
-        } catch {
-          case ioe: VirtualIOException =>
-            PartialPath(None, None)
-        }
-      }
-    } else {
-      val lastSlash = prefix.lastIndexOf('/')
-      if (lastSlash == -1) {
-        try {
-          val folder: Option[VirtualFolder] = currentFolder.findFolder(prefix, _.getCurrentUserPermission.execute)
-          if (folder.isDefined) {
-            PartialPath(folder, None)
-          } else {
-            PartialPath(Some(currentFolder), Some(prefix))
-          }
-        } catch {
-          case ioe: VirtualIOException =>
-            PartialPath(None, None)
-        }
-      } else {
-        try {
-          PartialPath(Some(currentFolder.resolveFolder(prefix.substring(0, lastSlash))),
-            if (prefix.length == lastSlash -1) None else Some(prefix.substring(lastSlash +1)))
-        } catch {
-          case ioe: VirtualIOException =>
-            PartialPath(None, None)
-        }
-      }
-    }
-  }
 }
