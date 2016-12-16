1330c92a1b33ab385aafdf12511446b3f99c49dc
Enrico Benedetti <enrico_benedetti@yahoo.com>
2016-12-16 13:47:08 +0100

    CdCommand now supports completion for relative and absolute paths.

--- src/main/scala/org/enricobn/shell/impl/CdCommand.scala
+++ src/main/scala/org/enricobn/shell/impl/CdCommand.scala
@@ -34,10 +34,78 @@ class CdCommand extends VirtualCommand {
     val parsedLine = new ParsedLine(line)
     val start = parsedLine.lastArgument.getOrElse("")
 
-    currentFolder.folders
-      .filter(_.getCurrentUserPermission.execute)
-      .map(_.name)
-      .filter(_.startsWith(start))
-      .toSeq
+    if (parsedLine.lastArgument.isDefined) {
+      val partialPath = resolveFolder(currentFolder, parsedLine.lastArgument.get)
+      println(partialPath)
+      if (partialPath.folder.isDefined) {
+        if (partialPath.remaining.isDefined) {
+          partialPath.folder.get.folders
+            .filter(_.getCurrentUserPermission.execute)
+            .filter(_.name.startsWith(partialPath.remaining.get))
+            .map(_.path + "/") // TODO if currentFolder I want the name
+            .toSeq
+        } else {
+          partialPath.folder.get.folders
+            .filter(_.getCurrentUserPermission.execute)
+            .map(_.path)
+            .toSeq
+        }
+      } else {
+        Seq.empty
+      }
+    } else {
+      currentFolder.folders
+        .filter(_.getCurrentUserPermission.execute)
+        .map(_.name)
+        .filter(_.startsWith(start))
+        .toSeq
+    }
+  }
+
+  case class PartialPath(folder: Option[VirtualFolder], remaining: Option[String]) {
+    // TODO implement VirtualNode.toString with path (can be final ?)
+    override def toString: String = "(" + folder.map(_.path).getOrElse("None") + "," + remaining + ")"
+  }
+
+  private def resolveFolder(currentFolder: VirtualFolder, prefix: String) : PartialPath = {
+    println(prefix)
+    if (prefix.startsWith("/")) {
+      val lastSlash = prefix.lastIndexOf('/')
+      if (lastSlash == 0) {
+        PartialPath(Some(currentFolder.root), Some(prefix.substring(lastSlash + 1)))
+      } else {
+        try {
+          // TODO currentPermission must be handled by resolveFolder
+          PartialPath(Some(currentFolder.resolveFolder(prefix.substring(0, lastSlash))),
+            if (prefix.length == lastSlash -1) None else Some(prefix.substring(lastSlash +1)))
+        } catch {
+          case ioe: VirtualIOException =>
+            PartialPath(None, None)
+        }
+      }
+    } else {
+      val lastSlash = prefix.lastIndexOf('/')
+      if (lastSlash == -1) {
+        try {
+          val folder: Option[VirtualFolder] = currentFolder.findFolder(prefix, _.getCurrentUserPermission.execute)
+          if (folder.isDefined) {
+            PartialPath(folder, None)
+          } else {
+            PartialPath(Some(currentFolder), Some(prefix))
+          }
+        } catch {
+          case ioe: VirtualIOException =>
+            PartialPath(None, None)
+        }
+      } else {
+        try {
+          PartialPath(Some(currentFolder.resolveFolder(prefix.substring(0, lastSlash))),
+            if (prefix.length == lastSlash -1) None else Some(prefix.substring(lastSlash +1)))
+        } catch {
+          case ioe: VirtualIOException =>
+            PartialPath(None, None)
+        }
+      }
+    }
   }
 }
