3b3dba15099c456dc183671a98d861d5b41fa8ea
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-10-06 00:09:27 +0200

    More on testing Shapeless: cleanup.

--- src/test/scala/TestingShapeless.scala
+++ src/test/scala/TestingShapeless.scala
@@ -1,5 +1,4 @@
-import shapeless.PolyDefns.~>
-import shapeless.ops.hlist.{Mapper, ToList}
+import shapeless.ops.hlist.Mapper
 import shapeless.{HList, HNil, Poly1}
 
 import scala.language.higherKinds
@@ -7,14 +6,7 @@ import scala.language.higherKinds
 object TestingShapeless {
 
   def main(args: Array[String]): Unit = {
-    val sets = Set(1) :: Set(1.1) :: HNil
-
-    val opts: HList = sets map choose
-
-    println(opts)
-
     parseArgs(StringArg() :: IntArg() :: HNil)
-
   }
 
   def parseArgs[IN <: HList, OUT <: HList, OUT1 <: HList](args: IN)
@@ -23,59 +15,13 @@ object TestingShapeless {
 
     val values = List("Hello", "world")
 
-//    var index = 0
-
-//    trait toArgValue extends Poly1 {
-//      private var index = 0
-//      implicit def default[Arg[T]] = at[Arg[_]](arg => {
-//        val value = if (values.length <= index) {
-//          None
-//        } else {
-//          Some(values(index))
-//        }
-//        index += 1
-//        ArgAndValue(arg, value)
-//
-//      })
-//    }
-
-
-//    val argAndValues = ArgAndValueString(StringArg(), Some("Hello")) :: ArgAndValueInt(IntArg(), Some("1")) :: HNil
-
     val argAndValues = args map toArgValue
 
     val parsed = argAndValues map parse
 
     println(argAndValues)
-
-
   }
 
-  def mapParse[IN <: HList, OUT <: HList](args: IN)(implicit mapper : Mapper.Aux[parse.type, IN, OUT]) : OUT = {
-    args map parse
-  }
-
-  def mapArgAndValue[IN <: HList, OUT <: HList](args: IN)(implicit mapper : Mapper.Aux[toArgValue.type, IN, OUT]) : OUT = {
-    args map toArgValue
-  }
-
-  //  private def mapArgAndValue[T <: HList](args : T)(implicit mapper : Mapper.Aux[toArgValue.type, T]) = {
-//    args map toArgValue
-//  }
-
-
-}
-
-class TestingShapeless[H <: HList](args: H)(implicit ktl: ToList[H, Set[_]]) {
-
-  def print(): Unit = {
-    println(args.toList)
-  }
-
-}
-
-object choose extends (Set ~> Option) {
-  def apply[T](s: Set[T]) = s.headOption
 }
 
 object toArgValue extends Poly1 {
@@ -83,34 +29,7 @@ object toArgValue extends Poly1 {
     at(arg => ArgAndValue(arg, None))
   implicit val caseInt : Case.Aux[IntArg,ArgAndValue[Int]] =
     at(arg => ArgAndValue(arg, None))
-
-//  implicit def all[T] : Case.Aux[Arg[T],ArgAndValue[T]] =
-//    at(arg => ArgAndValue(arg, None))
 }
-
-
-//object toArgValue extends (Arg ~> ArgAndValue) {
-//
-//  def apply[T](f: Arg[T]) = {
-//    //      var index = 0 // TODO
-//    //      val value = if (values.length <= index) {
-//    //        None
-//    //      } else {
-//    //        Some(values(index))
-//    //      }
-//    //      index += 1
-//    ArgAndValue(f, None)
-//  }
-//}
-
-
-//object parse extends (ArgAndValue ~> Option) {
-//  def apply[T](a: ArgAndValue[T]) =
-//    a.value match {
-//      case Some(value) => Some(a.arg.parse(value))
-//      case _ => None
-//    }
-//}
 
 object parse extends Poly1 {
   implicit val caseString : Case.Aux[ArgAndValue[String],Option[String]] =
@@ -126,8 +45,6 @@ object parse extends Poly1 {
 }
 
 case class ArgAndValue[T](arg: Arg[T], value: Option[String])
-
-
 
 sealed trait Arg[T] {
 
