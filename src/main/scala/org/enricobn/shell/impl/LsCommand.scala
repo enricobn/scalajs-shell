1139ee20ff17c301e5ce65db302b8500ed1f82d3
Enrico Benedetti <enrico_benedetti@yahoo.com>
2017-10-07 13:38:27 +0200

    Use VirtualCommandArguments in LsCommand.

--- src/main/scala/org/enricobn/shell/impl/LsCommand.scala
+++ src/main/scala/org/enricobn/shell/impl/LsCommand.scala
@@ -2,6 +2,7 @@ package org.enricobn.shell.impl
 
 import org.enricobn.shell.{RunContext, ShellInput, ShellOutput, VirtualCommand}
 import org.enricobn.vfs._
+import org.enricobn.vfs.IOError._
 
 import scala.scalajs.js.annotation.JSExport
 
@@ -21,16 +22,29 @@ object LsCommand {
 
 @JSExport(name = "LsCommand")
 class LsCommand extends VirtualCommand {
+
+  private val arguments = new VirtualCommandArguments(
+    // TODO filter?
+    FolderArgument("folder", false)
+  )
+
   def getName: String = "ls"
 
   override def run(shell: VirtualShell, shellInput: ShellInput, shellOutput: ShellOutput, args: String*) = {
-    val currentFolder: VirtualFolder = shell.currentFolder
+    val errorOrFolder = arguments.parse(shell.currentFolder, getName, args: _*) match {
+      case Left(error) => Left(IOError(error))
+      case Right(Seq(folder: VirtualFolder)) => Right(folder)
+      case Right(Seq()) => Right(shell.currentFolder)
+      case _ => "ls: illegal argument".ioErrorE
+    }
 
-    Right({
-      currentFolder.folders.right.get.foreach(folder => print(shellOutput, folder))
-      currentFolder.files.right.get.foreach(file => print(shellOutput, file))
-      new RunContext()
-    })
+    errorOrFolder match {
+      case Left(error) => Left(error)
+      case Right(folder) =>
+        folder.folders.right.get.foreach(f => print(shellOutput, f))
+        folder.files.right.get.foreach(file => print(shellOutput, file))
+        Right(new RunContext())
+    }
   }
 
   private def getAttributes(node: VirtualNode): String = {
@@ -43,6 +57,6 @@ class LsCommand extends VirtualCommand {
   }
 
   override def completion(line: String, currentFolder: VirtualFolder): Seq[String] = {
-    Seq.empty
+    arguments.complete(currentFolder, line)
   }
 }
